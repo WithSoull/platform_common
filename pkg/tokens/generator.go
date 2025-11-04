c6272d28e8057ffdb8b2a95f54e75e7949439408
WithSoull <withsoull@yandex.ru>
2025-11-04 16:00:08 +0300

    fix(deps): replace auth-service'models deps by platform models of UserClaims and UserInfo

--- pkg/tokens/generator.go
+++ pkg/tokens/generator.go
@@ -2,13 +2,11 @@ package tokens
 
 import (
 	"context"
-
-	"github.com/WithSoull/AuthService/internal/model"
 )
 
 type TokenGenerator interface {
-	GenerateAccessToken(context.Context, model.UserInfo) (string, error)
-	GenerateRefreshToken(context.Context, model.UserInfo) (string, error)
-	VerifyAccessToken(context.Context, string) (*model.UserClaims, error)
-	VerifyRefreshToken(context.Context, string) (*model.UserClaims, error)
+	GenerateAccessToken(context.Context, UserInfo) (string, error)
+	GenerateRefreshToken(context.Context, UserInfo) (string, error)
+	VerifyAccessToken(context.Context, string) (*UserClaims, error)
+	VerifyRefreshToken(context.Context, string) (*UserClaims, error)
 }
