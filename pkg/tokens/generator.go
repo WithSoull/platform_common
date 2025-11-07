72194f6ebdc154af0221d91160182b17fa18f56e
WithSoull <withsoull@yandex.ru>
2025-11-07 12:11:13 +0300

    feat(token-service): decompose token-service to tokenVerifier and tokenGenerator

--- pkg/tokens/generator.go
+++ pkg/tokens/generator.go
@@ -7,6 +7,14 @@ import (
 type TokenGenerator interface {
 	GenerateAccessToken(context.Context, UserInfo) (string, error)
 	GenerateRefreshToken(context.Context, UserInfo) (string, error)
+}
+
+type TokenVerifier interface {
 	VerifyAccessToken(context.Context, string) (*UserClaims, error)
 	VerifyRefreshToken(context.Context, string) (*UserClaims, error)
+}
+
+type TokenSevice interface {
+	TokenGenerator
+	TokenVerifier
 }
